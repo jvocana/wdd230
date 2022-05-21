55c878c8729d926529f5814293ca0d41ab5e6aa5
Jaxon <jaxonvocana@byui.edu>
2022-05-21 16:00:28 -0600

    finishing home page layout and dynamic banner

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -24,3 +24,41 @@ function toggleMenu() {
 
 document.querySelector('#menuButton').onclick = toggleMenu;
 
+
+// display banner
+
+const banner = document.querySelector('#banner')
+
+// test day
+const currDay = 1;
+
+function toggleBanner() {
+    
+}
+
+function displayBanner(text) {
+    if (currDay == 1 || currDay == 2) {
+        // creating elements
+        bannerMessage = document.createElement('div');
+        deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        // adding listening event to delete button
+        
+        // adding new elements to appropriate place
+        banner.appendChild(bannerMessage);
+        bannerMessage.appendChild(deleteButton);
+
+        // toggling class so banner message is displayed
+        banner.classList.toggle('showBanner');
+        deleteButton.addEventListener('click', deleteBanner => {
+            banner.classList.toggle('showBanner');
+
+        });
+    };
+    
+
+};
+
+
+
+displayBanner('🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.');
