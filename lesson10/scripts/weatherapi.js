65ab1f2e0d875d7e9743646949379ffb04c38f17
Jaxon <jaxonvocana@byui.edu>
2022-06-22 15:22:44 -0600

    implementing api activity

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,24 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=d4ecc5f5851ad237ea92914d71ff33e2'
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
