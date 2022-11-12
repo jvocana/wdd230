b36055cd28ad868471eca6767da11756e983b7f5
Jaxon <jaxonvocana@byui.edu>
2022-11-12 11:46:03 -0700

    implementing the prophets page

---
+++ lesson9/prophets.js
@@ -0,0 +1,58 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
+function displayProphets(prophet) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+    let birthDay = document.createElement('div');
+    let birthPlace = document.createElement('div');
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthDay.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    
+    
+    
+    if (prophet.order == 1) {
+        orderEnding = 'st';
+    }
+    else if (prophet.order == 2) {
+        orderEnding = 'nd';
+    }
+    else if (prophet.order == 3) {
+        orderEnding = 'rd';
+    }
+    else {
+        orderEnding = 'th';
+    };
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${orderEnding} Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+    
+    
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(birthDay);
+    card.appendChild(birthPlace);
+    card.appendChild(portrait);
+    
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+};
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
+  });
+
